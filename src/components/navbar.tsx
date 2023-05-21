a2142ac700b60ed52766d386436644035955d171
chi-wei-lien <wilsonwilson3124@gmail.com>
2023-05-21 10:45:05 -0400

    create navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+import '../style/navbar.css'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className='navbar-content'>
+          <h1>Problem Log</h1>
+          <h1>Study Group</h1>
+          <h1>Account</h1>
+        </div>  
+    </div>
+  );
+}
+
+export default Navbar;
